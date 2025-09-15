8099365ba15b2bcf7b4930bf5ea02a0da6801050
lckoff <camezlouca@gmail.com>
2025-09-15 22:12:16 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
 // REMPLACE ICI PAR TON URL PROJET (ex: "https://xxxx.supabase.co")
-const SUPABASE_URL = "https://TON_PROJECT_URL.supabase.co";
+const SUPABASE_URL = "https://zpzasjomecnrgwsbmamz.supabase.co";
 
 // Clé anon publique (OK côté navigateur) — fournie par toi
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwemFzam9tZWNucmd3c2JtYW16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5NjQ2NDEsImV4cCI6MjA3MzU0MDY0MX0.qwUpaOX2SsPTGm5EjpD1w5M_mNZ9NFPQPHyaDAdYb_c";
@@ -7,3 +7,4 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true }
 });
+
